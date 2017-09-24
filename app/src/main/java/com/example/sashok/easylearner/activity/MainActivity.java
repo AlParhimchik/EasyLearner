d437176be158783cc49fa933534c8a201144f26d
Sasha Parhimchik <sashok-15@tut.by>
2017-09-24 11:49:52 +0300

    add fragments

--- app/src/main/java/com/example/sashok/easylearner/activity/MainActivity.java
+++ app/src/main/java/com/example/sashok/easylearner/activity/MainActivity.java
@@ -1,4 +1,5 @@
-package com.example.sashok.easylearner.view;
+package com.example.sashok.easylearner.activity;
+
 
 import android.content.Context;
 import android.content.DialogInterface;
@@ -7,6 +8,8 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -22,6 +25,9 @@ import android.view.View;
 import android.widget.EditText;
 
 import com.example.sashok.easylearner.R;
+import com.example.sashok.easylearner.fragment.AddFolderFragment;
+import com.example.sashok.easylearner.fragment.SearchInNetFragment;
+import com.example.sashok.easylearner.fragment.ShowCardWithWords;
 import com.example.sashok.easylearner.model.Folder;
 import com.example.sashok.easylearner.model.RealmString;
 import com.example.sashok.easylearner.model.Word;
@@ -39,6 +45,15 @@ public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
     private NavigationView navigationView;
 
+    // tags used to attach the fragments
+    private static final String TAG_SHOW_CARD = "show_cards";
+    private static final String TAG_ADD_FOLDER = "addFolder";
+    private static final String TAG_SEARCH_INTERNER = "searchInternet";
+    public static String CURRENT_TAG = TAG_SHOW_CARD;
+
+    // index to identify current nav menu item
+    public static int navItemIndex = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,7 +62,51 @@ public class MainActivity extends AppCompatActivity {
         addMostFavFoldersToNav();
         setupToolBar();
         setListeners();
+
+        if (savedInstanceState == null) {
+            navItemIndex = 0;
+            CURRENT_TAG = TAG_SHOW_CARD;
+            loadFragment();
+        }
     }
+
+    public void loadFragment() {
+        // if user select the current navigation menu again, don't do anything
+        // just close the navigation drawer
+        if (getSupportFragmentManager().findFragmentByTag(CURRENT_TAG) != null) {
+            drawerLayout.closeDrawers();
+            toggleFab();
+            return;
+        }
+        Fragment fragment = getFragment();
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.setCustomAnimations(android.R.anim.fade_in,
+                android.R.anim.fade_out);
+        fragmentTransaction.replace(R.id.frame_layout, fragment,CURRENT_TAG);
+        fragmentTransaction.commit();
+        toggleFab();
+        drawerLayout.closeDrawers();
+
+    }
+
+    private Fragment getFragment() {
+        switch (navItemIndex) {
+            case 0:
+
+                ShowCardWithWords cardFragment = new ShowCardWithWords();
+                return cardFragment;
+            case 1:
+
+                AddFolderFragment addFolderFragment = new AddFolderFragment();
+                return addFolderFragment;
+            case 2:
+                SearchInNetFragment searchFragment = new SearchInNetFragment();
+                return searchFragment;
+            default:
+                return new ShowCardWithWords();
+        }
+
+        }
 
 
     public void initialize() {
@@ -78,6 +137,29 @@ public class MainActivity extends AppCompatActivity {
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    //Replacing the main content with ContentFragment Which is our Inbox View;
+                    case R.id.add_folder:
+                        navItemIndex = 1;
+                        CURRENT_TAG = TAG_ADD_FOLDER;
+                        break;
+                    case R.id.net_search:
+                        navItemIndex = 2;
+                        CURRENT_TAG = TAG_ADD_FOLDER;
+                        break;
+
+
+                    default:
+                        navItemIndex = 0;
+                }
+                if (item.isChecked()) {
+                    item.setChecked(false);
+                } else {
+                    item.setChecked(true);
+                }
+                item.setChecked(true);
+
+                loadFragment();
                 drawerLayout.closeDrawer(GravityCompat.START);
                 return true;
             }
@@ -89,6 +171,22 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+//    public void setFragment(int item_id) {
+//        Fragment fragment;
+//        switch (item_id) {
+//            case R.id.add_folder:
+//                fragment = new AddFolderFragment();
+//                break;
+//            case R.id.net_search:
+//                fragment = new SearchInNetFragment();
+//                break;
+//        }
+//        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+//        fragmentTransaction.setCustomAnimations(android.R.anim.fade_in,
+//                android.R.anim.fade_out);
+//        fragmentTransaction.replace(R.id.frame, fragment, CURRENT_TAG);
+//    }
 
     public void setupToolBar() {
 
@@ -139,13 +237,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void addMostFavFoldersToNav() {
-        NavigationView navView = (NavigationView) findViewById(R.id.nav_view);
         RealmController realmController = RealmController.with(MainActivity.this);
         List<Folder> folders = realmController.getFolders();
         int count = 0;
         if (folders != null) {
             for (Folder folder : folders) {
-                Menu m = navView.getMenu();
+                Menu m = navigationView.getMenu();
                 MenuItem item = m.getItem(0);
                 SubMenu subMenu = item.getSubMenu();
                 subMenu.add(folder.getName()).setIcon(R.drawable.ic_action_black_folder);
@@ -162,7 +259,7 @@ public class MainActivity extends AppCompatActivity {
         super.onPostCreate(savedInstanceState);
         // Синхронизировать состояние переключения после того, как
         // возникнет onRestoreInstanceState
-       // actionBarDrawerToggle.syncState();
+        // actionBarDrawerToggle.syncState();
     }
 
     @Override
@@ -178,17 +275,31 @@ public class MainActivity extends AppCompatActivity {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
+        }
+        if (navItemIndex != 0) {
+            navItemIndex = 0;
+            CURRENT_TAG = TAG_SHOW_CARD;
+            loadFragment();
+            return;
         } else {
             super.onBackPressed();
         }
     }
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
-//        return true;
-//    }
+    public void toggleFab() {
+        if (navItemIndex == 0) {
+            fab.show();
+        } else {
+            fab.hide();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -198,7 +309,10 @@ public class MainActivity extends AppCompatActivity {
 
             return true;
         }
-
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
         return super.onOptionsItemSelected(item);
     }
+
 }
---
+++ app/src/main/java/com/example/sashok/easylearner/fragment/SearchInNetFragment.java
@@ -0,0 +1,10 @@
+package com.example.sashok.easylearner.fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by sashok on 24.9.17.
+ */
+
+public class SearchInNetFragment extends Fragment {
+}
